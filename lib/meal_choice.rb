b82f9ec149fbb3269cb2aa840c06b6b9dbbee4b5
Demarius Edwards <demarius.edwards@gmail.com>
2019-11-27 16:33:11 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,7 +3,10 @@
 #arg -> 3 args, 1 default -- veg 1, veg 2, protein(default)
 #puts "What a nutritious..."
 #puts "A plate of #{protein} with #{veg 1 & 2}"
+#return by calling the method
+
 def meal_choice(veg1, veg2, protein = "meat")
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
+meal_choice(veg1, veg2, protein = "meat")
